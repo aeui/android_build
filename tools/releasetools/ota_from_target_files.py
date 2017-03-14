18ce47d55992244f85ee6dcd178cce59b990089b
moludo <moledo.alex@gmail.com>
2017-03-14 23:51:06 +0100

    Add Nuclear Info on flash

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -668,6 +668,62 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
 
   system_progress = 0.75
 
+  script.Print("")
+  script.Print(" _____      ___     ______________   ")
+  script.Print("|     \    |   |   |     ______   |  ")
+  script.Print("|      \   |   |   |    |      |  |  ")
+  script.Print("|       \  |   |   |    |______|  |  ")
+  script.Print("|        \ |   |   |    |_________|  ")
+  script.Print("|    |\   \|   |   |    | \    \     ")
+  script.Print("|    | \       |   |    |  \    \    ")
+  script.Print("|    |  \      |   |    |   \    \   ")
+  script.Print("|    |   \     |   |    |    \    \  ")
+  script.Print("|____|    \____|   |____|     \____\ ")
+  script.Print("")
+  script.Print("")
+  script.Print("      http://nuclearom.com    ")
+
+
+  if GetBuildProp("ro.modversion", OPTIONS.info_dict) is not None:
+    buildid = GetBuildProp("ro.modversion", OPTIONS.info_dict)
+    buildtype = GetBuildProp("ro.cm.releasetype", OPTIONS.info_dict)
+    buildidn = GetBuildProp("ro.build.id", OPTIONS.info_dict)
+    buildday = GetBuildProp("ro.build.date", OPTIONS.info_dict)
+    securep = GetBuildProp("ro.build.version.security_patch", OPTIONS.info_dict)
+    buildhst = GetBuildProp("ro.build.host", OPTIONS.info_dict)
+    density = GetBuildProp("ro.sf.lcd_density", OPTIONS.info_dict)
+    device = GetBuildProp("ro.product.name", OPTIONS.info_dict)
+    androidver = GetBuildProp("ro.build.version.release", OPTIONS.info_dict)
+    manufacturer = GetBuildProp("ro.product.manufacturer", OPTIONS.info_dict)
+    maintainer = GetBuildProp("ro.build.user", OPTIONS.info_dict)
+    sdkver = GetBuildProp("ro.build.version.sdk", OPTIONS.info_dict)
+    script.Print(" **************** Software *****************");
+    script.Print(" OS ver: %s"%(buildid));
+    script.Print("");
+    script.Print(" Android ver: %s"%(androidver));
+    script.Print("");
+    script.Print(" Security patch: %s"%(securep));
+    script.Print("");
+    script.Print(" SDK ver: %s"%(sdkver));
+    script.Print("");
+    script.Print(" Build ID: %s"%(buildidn));
+    script.Print("");
+    script.Print(" Build date: %s"%(buildday));
+    script.Print("");
+    script.Print(" Build type: %s"%(buildtype));
+    script.Print("");
+    script.Print(" Build host: %s"%(buildhst));
+    script.Print("");
+    script.Print(" Maintainer: %s"%(maintainer));
+    script.Print(" **************** Hardware *****************");
+    script.Print(" Device codename: %s"%(device));
+    script.Print("");
+    script.Print(" Manufacturer: %s"%(manufacturer));
+    script.Print("");
+    script.Print(" LCD density: %s"%(density));
+    script.Print("");
+    script.Print(" *******************************************");
+  
   if OPTIONS.wipe_user_data:
     system_progress -= 0.1
   if HasVendorPartition(input_zip):
@@ -682,8 +738,6 @@ else if get_stage("%(bcb_dev)s") == "3/3" then
     script.Unmount("/data")
     script.AppendExtra("endif;")
 
-  # Place a copy of file_contexts.bin into the OTA package which will be used
-  # by the recovery program.
   if "selinux_fc" in OPTIONS.info_dict:
     WritePolicyConfig(OPTIONS.info_dict["selinux_fc"], output_zip)
 
